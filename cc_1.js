ef943c0f0e36e919447427cb15063e3e94686e50
Haferm98 <105403350+Haferm98@users.noreply.github.com>
2024-08-31 11:05:55 -0400

    Task 1: Variables and Data Types

---
+++ cc_1.js
@@ -0,0 +1,9 @@
+// Task 1: Variables and Data Types
+
+let employeeName = "Jon Snow";
+const employeeID = 101;
+var isActive = true;
+
+console.log(employeeName, typeof(employeeName));
+console.log(employeeID, typeof(employeeID));
+console.log(isActive, typeof(isActive));
