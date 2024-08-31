d5d83aacc28a063f4d33ac5e24b6a135e964c684
Haferm98 <105403350+Haferm98@users.noreply.github.com>
2024-08-31 11:17:51 -0400

    Task 2: Compound Data Types

--- cc_1.js
+++ cc_1.js
@@ -7,3 +7,16 @@ var isActive = true;
 console.log(employeeName, typeof(employeeName));
 console.log(employeeID, typeof(employeeID));
 console.log(isActive, typeof(isActive));
+
+
+
+// Task 2: Compound Data Types
+
+let products = ["DragonGlass", "Crown", "DireWolf"];
+const productDetails = {
+    productName: products,
+    price: 99.99,
+    inStock: true,
+}
+
+console.log(products, productDetails);
